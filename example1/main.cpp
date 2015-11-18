1b7ff609a2aef95bbec2977e3099b9fd6baca6cf
18404715032 <20141105066yh@admindeimac-36.local>
2015-11-18 21:06:04 +0800

    Initial Commit

---
+++ example1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  example1
+//
+//  Created by 杨浩 on 15/11/18.
+//  Copyright © 2015年 杨浩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
